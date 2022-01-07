80959e63facbab44066027d08985c393f2c6ebbf
Felix Reissmann <felix.rm.153@gmail.com>
2022-01-07 11:05:07 +0100

    Moved tests for lw_std to lightweight_std repository

--- tests/testing.cpp
+++ tests/testing.cpp
@@ -7,27 +7,12 @@
 #include "logging.hpp"
 #include "test_disconnect_reconnect.hpp"
 #include "test_logging.hpp"
-#include "test_lw_list.hpp"
-#include "test_lw_pair.hpp"
-#include "test_lw_queue.hpp"
-#include "test_lw_unordered_map.hpp"
-#include "test_lw_unordered_set.hpp"
-#include "test_lw_vector.hpp"
 #include "test_network_building.hpp"
 #include "test_network_visualization.hpp"
 #include "test_send_receive.hpp"
 
 int main() {
     iac::Logging::loglevel = iac::Logging::loglevels::network;
-
-    TestLogging::start_suite("lw_std");
-
-    TestLogging::run<TestLwVector>();
-    TestLogging::run<TestLwList>();
-    TestLogging::run<TestLwPair>();
-    TestLogging::run<TestLwQueue>();
-    TestLogging::run<TestLwUnorderedSet>();
-    TestLogging::run<TestLwUnorderedMap>();
 
     TestLogging::start_suite("communication");
 
